af1680e467a83ddb40284f3bcd89b3c1332075cd
Laura Berge <lauraeberge@gmail.com>
2019-09-09 20:29:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,12 +5,10 @@ def prime?(n)
   elsif n <= 1
     false
   else
-    until i == n do
+    if i < n do
       if n % i == 0
         return false
         break
-      else
-        true
       end
       i += 1
     end
