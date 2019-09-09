cd1cbdef59fef3e9985ed3c3e15a4bd69cd8e7b1
Laura Berge <lauraeberge@gmail.com>
2019-09-09 20:25:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,18 @@
 def prime?(n)
   divisor = 2
-  until divisor == n do
-    if n % divisor == 0
-      false
-      break
-    else
-      true
+  if n.between?(1, 3)
+    true
+  elsif n <= 0
+    false
+  else
+    until divisor == n do
+      if n % divisor == 0
+        false
+        break
+      else
+        true
+      end
+      divisor += 1
     end
-    divisor += 1
   end
 end
